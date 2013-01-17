26d6367cb0b8b4349e405f8df77fea0cfc8e2918
Jason Ekstrand <jason@jlekstrand.net>
2013-01-16 19:57:10 -0600

    Make the android library build completely including JNI

--- jni/external/libffi.mk
+++ jni/external/libffi.mk
@@ -1,5 +1,9 @@
 LIBFFI_PATH := $(call my-dir)/libffi
 
+# We need this because what gets tacked on to the end of it is
+# external/libffi/filename
+LIBFFI_INCLUDE_PREFIX := $(call my-dir)/..
+
 # Architecture is auto-detected. On android, TARGET_OS is not defined so we set
 # it to linux
 ffi_arch := $(TARGET_ARCH)
@@ -12,8 +16,9 @@ include $(LIBFFI_PATH)/Libffi.mk
 
 # We have to convert the include paths to be global paths in our directory
 # structure
-LOCAL_C_INCLUDES := $(foreach path, $(LOCAL_C_INCLUDES), $(LOCAL_PATH)/$(path))
-LOCAL_SRC_FILES := $(foreach path, $(LOCAL_SRC_FILES), external/libffi/$(path))
+LOCAL_SRC_FILES := $(foreach path, $(LOCAL_SRC_FILES), $(LIBFFI_PATH)/$(path))
+
+LOCAL_C_INCLUDES := $(foreach path, $(LOCAL_C_INCLUDES), $(LIBFFI_INCLUDE_PREFIX)/$(path))
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
 
--- jni/files.mk
+++ jni/files.mk
@@ -18,21 +18,18 @@
 # TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
 # OF THIS SOFTWARE.
 
-LOCAL_PATH := $(call my-dir)
+WAYLAND_JNI_UTIL_SRC := \
+	src/object.c \
+	src/interface.c \
+	src/fixed.c \
+	src/wayland-jni.c
 
-include $(LOCAL_PATH)/external/libffi.mk
-include $(LOCAL_PATH)/external/wayland.mk
-
-# Begin local wayland-java build
-include $(LOCAL_PATH)/files.mk
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE 			:= libwayland-java-server
-LOCAL_CFLAGS			:= -I$(LOCAL_PATH)/src
-LOCAL_SRC_FILES 		:= $(foreach file, $(WAYLAND_JNI_SERVER_SRC), $(LOCAL_PATH)/$(file))
-LOCAL_LDLIBS 			:= -llog
-LOCAL_STATIC_LIBRARIES	:= libwayland-server
-
-include $(BUILD_SHARED_LIBRARY)
+WAYLAND_JNI_SERVER_SRC := $(WAYLAND_JNI_UTIL_SRC) \
+	src/server/display.c \
+	src/server/global.c \
+	src/server/client.c \
+	src/server/event_loop.c \
+	src/server/resource.c \
+	src/server/listener.c \
+	src/server/protocol.c
 
