e3d9996c1dfad6e4b47745c59b99179d39f76380
Jason Ekstrand <jason@jlekstrand.net>
2013-02-20 21:43:05 -0600

    Add a very basic EventLoop test

---
+++ test/src/org/freedesktop/wayland/server/EventLoopTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright © 2012-2013 Jason Ekstrand.
+ *  
+ * Permission to use, copy, modify, distribute, and sell this software and its
+ * documentation for any purpose is hereby granted without fee, provided that
+ * the above copyright notice appear in all copies and that both that copyright
+ * notice and this permission notice appear in supporting documentation, and
+ * that the name of the copyright holders not be used in advertising or
+ * publicity pertaining to distribution of the software without specific,
+ * written prior permission.  The copyright holders make no representations
+ * about the suitability of this software for any purpose.  It is provided "as
+ * is" without express or implied warranty.
+ * 
+ * THE COPYRIGHT HOLDERS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+ * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO
+ * EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
+ * DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+ * OF THIS SOFTWARE.
+ */
+package org.freedesktop.wayland.server;
+
+import org.junit.*;
+
+public class EventLoopTest
+{
+    EventLoop loop;
+    boolean called;
+
+    public EventLoopTest()
+    { }
+
+    @Before
+    public void createDisplay()
+    {
+        loop = new EventLoop();
+    }
+
+    @Test
+    public void testIdle()
+    {
+        called = false;
+
+        loop.addIdle(new EventLoop.IdleHandler() {
+            public void handleIdle()
+            {
+                called = true;
+            }
+        });
+
+        loop.dispatchIdle();
+
+        Assert.assertTrue(called);
+    }
+
+    @After
+    public void destroyDisplay()
+    {
+    }
+}
+
